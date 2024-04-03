d6cd57ea3308ddc34be1cfba0726d1249b06aa8d
aikawa <aikawa9376@gmail.com>
2024-04-03 09:11:49 +0900

    更に便利

--- keyball39/keymaps/default/features/combo.h
+++ keyball39/keymaps/default/features/combo.h
@@ -13,6 +13,7 @@ enum combos {
     CM_DF,
     CM_CV,
     CM_MV,
+    CM_MS,
     CM_LC,
     CM_RC,
     CM_TG,
@@ -24,6 +25,7 @@ const uint16_t PROGMEM imeon_combo[] = {KC_C, KC_V, COMBO_END};
 // const uint16_t PROGMEM rctrl_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM rctrl_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM lctrl_combo[] = {KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM spacem_combo[] = {KC_OG_BTN1, KC_OG_BTN2, COMBO_END};
 const uint16_t PROGMEM imeonm_combo[] = {KC_OG_BTN4, KC_OG_BTN5, COMBO_END};
 
 // test
@@ -37,4 +39,5 @@ combo_t key_combos[] = {
     [CM_RC] = COMBO(lctrl_combo, MO(3)),
     [CM_TG] = COMBO(stogg_combo, CW_TOGG),
     [CM_MV] = COMBO(imeonm_combo, KC_RGUI),
+    [CM_MS] = COMBO(spacem_combo, KC_SPACE),
 };
